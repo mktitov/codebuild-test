c776ea62e2fd3867d848fca9290cf5b68edbe31e
titov-clearscale <mikhail.titov@clearscale.net>
2020-03-14 15:21:48 +0300

    Initial commit

---
+++ src/test/java/org/tim/SimpleTest.java
@@ -0,0 +1,11 @@
+package org.tim;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
